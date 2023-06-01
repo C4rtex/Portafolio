ed7da3cc045bac7db814075eec497d40c52c7e9d
C4rtex <carlos.jimenezc@cun.edu.co>
2023-06-01 13:08:50 -0500

    primer commit avanzado hasta inyección de dependencias, de momento con clase y la próxima con interface

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,41 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto()
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link = "https://amazon.com",
+                    ImagenURL = "/imagenes/amazon.png"
+                },
+                new Proyecto()
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "pagina noticias en react",
+                    Link = "https://nytimes.com",
+                    ImagenURL = "/imagenes/nyt.PNG"
+                },new Proyecto()
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "red social para compartir en comunidades",
+                    Link = "https://reddit.com",
+                    ImagenURL = "/imagenes/reddit.PNG"
+                }
+                ,new Proyecto()
+                {
+                    Titulo = "Steam",
+                    Descripcion = "tienda en linea para comprar videojuegos",
+                    Link = "https://store.steampowered.com",
+                    ImagenURL = "/imagenes/steam.PNG"
+                }
+            };
+        }
+    }
+}
